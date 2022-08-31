58de0589163c29bd003809ecb2080c64e4deca46
Piotr Ostrowski <piotrostr@google.com>
2022-08-31 22:18:00 +0200

    feat: implement a base grpc server

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/piotrostr/oauth2-grpc/api"
+	pb "github.com/piotrostr/oauth2-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	authService := api.NewAuthService()
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterAuthServiceServer(grpcServer, authService)
+	log.Println("Serving on port 50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("error while serving: %v", err)
+	}
+	// TODO use ServeHTTP instead of Serve to provide both REST and gRPC
+}
