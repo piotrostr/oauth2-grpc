c643c102453e702ac3a568109a21c8c72d0b359d
Piotr Ostrowski <piotrostr@google.com>
2022-08-31 22:36:13 +0200

    feat: implement the service methods

--- api/server.go
+++ api/server.go
@@ -2,33 +2,56 @@ package api
 
 import (
 	"context"
+	"errors"
 	"sync"
+	"time"
 
 	pb "github.com/piotrostr/oauth2-grpc/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+func GetToken() *pb.Token {
+	return &pb.Token{
+		AccessToken: "access_token",
+		ExpiresAt:   timestamppb.New(time.Now().Add(time.Hour)),
+	}
+}
 
 // TODO add database connection
 type AuthService struct {
 	pb.UnimplementedAuthServiceServer
 
-	mu          sync.Mutex
-	userDetails map[string]*pb.UserDetails
+	mu    sync.Mutex
+	users map[string]*pb.UserDetails
 }
 
 var ctx = context.Background()
 
 func NewAuthService() *AuthService {
 	return &AuthService{
-		userDetails: make(map[string]*pb.UserDetails),
+		users: make(map[string]*pb.UserDetails),
 	}
 }
 
-// TODO implement
-func (s *AuthService) CreateAccount(ctx context.Context, userDetails *pb.UserDetails) (*pb.Token, error) {
-	return nil, nil
+func (s *AuthService) CreateAccount(
+	ctx context.Context,
+	userDetails *pb.UserDetails,
+) (*pb.Token, error) {
+	s.mu.Lock()
+	s.users[userDetails.Credentials.Username] = userDetails
+	s.mu.Unlock()
+	return GetToken(), nil
 }
 
-// TODO implement
-func (s *AuthService) Authenticate(ctx context.Context, credentials *pb.Credentials) (*pb.Token, error) {
-	return nil, nil
+func (s *AuthService) Authenticate(
+	ctx context.Context,
+	credentials *pb.Credentials,
+) (*pb.Token, error) {
+	for _, user := range s.users {
+		if user.Credentials.Username == credentials.Username &&
+			user.Credentials.Password == credentials.Password {
+			return GetToken(), nil
+		}
+	}
+	return nil, errors.New("invalid credentials")
 }
