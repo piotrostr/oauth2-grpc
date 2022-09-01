26ceed285c4316af3414787e7a74ae68bffea3b9
Piotr Ostrowski <piotrostr@google.com>
2022-09-01 14:38:35 +0200

    feat: implement the client of AuthService

--- api/client.go
+++ api/client.go
@@ -1,4 +1,33 @@
 package api
 
-// TODO implement client
-func _() {}
+import (
+	"log"
+
+	pb "github.com/piotrostr/oauth2-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+type Client struct {
+	conn       *grpc.ClientConn
+	authClient pb.AuthServiceClient
+}
+
+func NewClient(url string) *Client {
+	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+	return &Client{
+		conn:       conn,
+		authClient: pb.NewAuthServiceClient(conn),
+	}
+}
+
+func (c *Client) Authenticate(credentials *pb.Credentials) {
+	c.Authenticate(credentials)
+}
+
+func (c *Client) CreateAccount(userDetails *pb.UserDetails) {
+	c.CreateAccount(userDetails)
+}
