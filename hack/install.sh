5bec087024511a7ae05ed12a926af872dbc9c4a1
Piotr Ostrowski <piotrostr@google.com>
2022-09-01 11:19:15 +0200

    feat: install grpc-gateway plugin along the go-grpc plugins

--- hack/install.sh
+++ hack/install.sh
@@ -2,3 +2,4 @@
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
